00d8910a4a517e8f58249f5b8d76042b3e9a31ba
Your Name <you@example.com>
2024-02-29 14:56:31 +0800

    update

--- git-push.sh
+++ git-push.sh
@@ -1,3 +1,5 @@
 # rsync -avzP --exclude=node_modules /mnt/e/clash/test/blog/ /mnt/e/clash/test/marklinglon.github.io/
 # cd /mnt/e/clash/test/marklinglon.github.io/
 git add *;git commit -m "update";git push origin main:main
+# deploy
+hexo clean ;hexo generate;hexo deploy
